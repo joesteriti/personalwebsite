697f840aaa633c99d122f2bfde6de697569686e2
joesteriti <113952410+joesteriti@users.noreply.github.com>
2024-03-05 18:10:32 -0500

    Complete project

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,11 @@
+import Recycle from "../images/recycle.png"
+
+export const ProjectList = [
+    {
+            name: "Sustainability Track (BostonHacks2023)",
+            image: Recycle,
+            skills: "JavaScript, GoogleAPI",
+            link: "https://github.com/Jz8801/SustainaBucks",
+    },
+
+];
